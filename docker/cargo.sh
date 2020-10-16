b92e9fd26fc0ea3c64f25a0a7ee25c3749b7c006
Markus Reiter <me@reitermark.us>
2020-10-17 01:51:26 +0200

    Fix installing `cargo`.

--- docker/cargo.sh
+++ docker/cargo.sh
@@ -25,7 +25,7 @@ main() {
     sh rustup-init.sh -y --no-modify-path --profile minimal
     rm rustup-init.sh
 
-    PATH="${CARGO_HOME}/bin:${PATH}" cargo install --git https://github.com/reitermarkus/cargo --branch linkarg --root /usr/local
+    PATH="${CARGO_HOME}/bin:${PATH}" cargo install --git https://github.com/reitermarkus/cargo --branch linkarg --root /usr/local cargo
 
     rm -r "${RUSTUP_HOME}" "${CARGO_HOME}"
 
